a7ac83df833970846b12a1f88054ffebc0a918be
Jordan Farnworth <jordanfarn23@gmail.com>
2016-05-19 18:19:06 -0600

    "updating master"

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-bundle exec rake assets:precompile ENV=production
 git add -u
 git commit -m \""$1"\"
 git push $2 master -f
+
+git add -A
+git reset --hard
+
+bundle exec rake assets:precompile ENV=production
+git add -A
+git commit -m 'assets'
+git push $2 master
